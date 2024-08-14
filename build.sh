0ffe0030e04e88e896fedf4421819487086148fe
Raihan Rafid <raihanrfd1@gmail.com>
2024-08-14 22:42:56 +0700

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 git pull origin master
-/root/.nvm/versions/node/v22.6.0/bin/pm2 delete twitter-api
-/root/.nvm/versions/node/v22.6.0/bin/pm2 start server.js -n twitter-api
+pm2 delete twitter-api
+pm2 start server.js -n twitter-api

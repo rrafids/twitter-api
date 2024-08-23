e20ae1666cac76d117c4597382115e9671845ffb
Raihan Rafid <raihanrfd1@gmail.com>
2024-08-23 20:40:33 +0700

    add socket io

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./src/app');
 const PORT = 3001;
 
+// TODO: integrate with socket.io
+// var io = require('socket.io')(app);
+
 app.listen(PORT, function () {
   console.log(`Server berjalan pada http://localhost:${PORT}`);
 });
